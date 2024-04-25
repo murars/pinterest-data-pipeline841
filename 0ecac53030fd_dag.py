e918eda8ff6f1fef548e56de9f5fc0f7bb3638e1
murars <marslan72@gmail.com>
2024-04-25 15:08:14 +0100

    Create '0ecac53030fd_dag.py' to orchestrate Databricks notebook jobs for data processing

---
+++ 0ecac53030fd_dag.py
@@ -0,0 +1,42 @@
+from airflow import DAG
+from airflow.providers.databricks.operators.databricks import DatabricksSubmitRunOperator, DatabricksRunNowOperator
+from datetime import datetime, timedelta 
+
+
+#Define params for Submit Run Operator
+notebook_task = {
+    'notebook_path': '/Users/marslan72@gmail.com/clean_and_queries_batch_data.ipynb',
+}
+
+#Define params for Run Now Operator
+notebook_params = {
+    "Variable":5
+}
+
+default_args = {
+    'owner': 'MURAT ARSLAN',
+    'depends_on_past': False,
+    'email_on_failure': False,
+    'email_on_retry': False,
+    'retries': 1,
+    'retry_delay': timedelta(minutes=2)
+}
+
+
+with DAG('0ecac53030fd_dag',
+    # should be a datetime format
+    start_date=datetime(2024, 4, 25),
+    # check out possible intervals, should be a string
+    schedule_interval='@daily',
+    catchup=False,
+    default_args=default_args
+    ) as dag:
+
+    opr_submit_run = DatabricksSubmitRunOperator(
+        task_id='submit_run',
+        # the connection we set-up previously
+        databricks_conn_id='databricks_default',
+        existing_cluster_id='1108-162752-8okw8dgg',
+        notebook_task=notebook_task
+    )
+    opr_submit_run
